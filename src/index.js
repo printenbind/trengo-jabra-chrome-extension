fc6329885a4cdb587c9e10904d6cec30a734bbda
Jelle Breuer <jelle@breuclan.nl>
2022-03-15 15:53:08 +0100

    V1

--- src/index.js
+++ src/index.js
@@ -9,16 +9,53 @@ const waitForPickup = async () => {
 
   api.deviceList.subscribe((devices) => {
     devices.forEach(async (device) => {
+        console.log(`Phone found ${device.name} - ${device.productId}`)
         const deviceCallControl = await callControlFactory.createCallControl(device);
         const gotLock = await deviceCallControl.takeCallLock();
 
-        const signalSubscription = deviceCallControl.deviceSignals.subscribe((signal) => {
+        if (gotLock) {
+          console.log('Phone lock successfull')
+          deviceCallControl.deviceSignals.subscribe((signal) => {
+            console.log(SignalType[signal.type], signal)
             if (signal.value && SignalType.HOOK_SWITCH === signal.type) {
-                alert('pickup2')
+                onPickup()
             }
-        });
+          });
+        } else {
+          console.log('Failed to get phone lock')
+        }
     });
   });
 }
 
-waitForPickup()
+const onPickup = () => {
+  console.log('Phone hook trigger')
+  let pickupUpButton = null;
+  let hangUpButton = null;
+
+  const collection = document.getElementsByClassName("material-icons");
+  for (let item of collection) {
+    if (item.textContent === 'call') {
+      if (item.offsetParent !== null) {
+        pickupUpButton = item.parentNode
+      }
+    }
+    if (item.textContent === 'call_end') {
+      if (item.offsetParent !== null && item.parentNode.classList.contains('call-action-icon-red')) {
+        hangUpButton = item.parentNode
+      }
+    }
+  }
+
+  if (pickupUpButton) {
+    pickupUpButton.click()
+    console.log('Pickup successfull')
+  } else if (hangUpButton) {
+    hangUpButton.click()
+    console.log('Hangup successfull')
+  } else {
+    console.log('Phone window not visible')
+  }
+}
+
+waitForPickup();
